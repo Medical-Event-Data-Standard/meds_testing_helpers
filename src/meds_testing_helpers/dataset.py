e6689968961db72d5d2fb455b98c3ffe0b938b1e
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-03 12:57:59 -0500

    Unified CSV parsing into a single code path leveraging defaults.

--- src/meds_testing_helpers/dataset.py
+++ src/meds_testing_helpers/dataset.py
@@ -148,6 +148,52 @@ class MEDSDataset:
         >>> D.shard_fps is None
         True
 
+    Note that code metadata can be inferred to be empty if not provided:
+        >>> print(MEDSDataset(data_shards=data_shards, dataset_metadata=dataset_metadata))
+        MEDSDataset:
+        dataset_metadata:
+          - dataset_name: test
+          - dataset_version: 0.0.1
+          - etl_name: foo
+          - etl_version: 0.0.1
+          - meds_version: 0.3.3
+          - created_at: 1/1/2025
+          - extension_columns: []
+        data_shards:
+          - 0:
+            pyarrow.Table
+            subject_id: int64
+            time: timestamp[us]
+            code: string
+            numeric_value: float
+            ----
+            subject_id: [[0]]
+            time: [[1970-01-01 00:00:00.000000]]
+            code: [["A"]]
+            numeric_value: [[null]]
+          - 1:
+            pyarrow.Table
+            subject_id: int64
+            time: timestamp[us]
+            code: string
+            numeric_value: float
+            ----
+            subject_id: [[1]]
+            time: [[1970-01-01 00:00:00.000000]]
+            code: [["B"]]
+            numeric_value: [[1]]
+        code_metadata:
+          pyarrow.Table
+          code: string
+          description: string
+          parent_codes: list<item: string>
+            child 0, item: string
+          ----
+          code: []
+          description: []
+          parent_codes: []
+        subject_splits: None
+
     You can save and load datasets from disk in the proper format. Note that equality persists after this
     operation:
         >>> import tempfile
@@ -338,13 +384,9 @@ class MEDSDataset:
         Traceback (most recent call last):
             ...
         ValueError: dataset_metadata must be provided if root_dir is None
-        >>> MEDSDataset(data_shards=data_shards, dataset_metadata=dataset_metadata)
-        Traceback (most recent call last):
-            ...
-        ValueError: code_metadata must be provided if root_dir is None
     """
 
-    DEFAULT_CSV_TS_FORMAT = "%m/%d/%Y, %H:%M:%S"
+    CSV_TS_FORMAT = "%m/%d/%Y, %H:%M:%S"
 
     PL_DATA_SCHEMA = {
         subject_id_field: pl.Int64,
@@ -364,6 +406,17 @@ class MEDSDataset:
         "split": pl.String,
     }
 
+    PL_LABEL_SCHEMA = {
+        subject_id_field: pl.Int64,
+        prediction_time_field: pl.Datetime("us"),
+        "boolean_value": pl.Boolean,
+        "integer_value": pl.Int64,
+        "float_value": pl.Float64,
+        "categorical_value": pl.String,
+    }
+
+    TIME_FIELDS = {time_field, prediction_time_field}
+
     def __init__(
         self,
         root_dir: Path | None = None,
@@ -378,7 +431,11 @@ class MEDSDataset:
             if dataset_metadata is None:
                 raise ValueError("dataset_metadata must be provided if root_dir is None")
             if code_metadata is None:
-                raise ValueError("code_metadata must be provided if root_dir is None")
+                logger.warning("Inferring empty code metadata as none was provided.")
+                code_metadata = pl.DataFrame(
+                    {code_field: [], description_field: [], parent_codes_field: []},
+                    schema=self.PL_CODE_METADATA_SCHEMA,
+                )
 
         self.root_dir = root_dir
         self.data_shards = data_shards
@@ -393,24 +450,24 @@ class MEDSDataset:
         self.dataset_metadata
 
     @classmethod
-    def _parse_csv(cls, csv: str, **schema) -> pl.DataFrame:
+    def parse_csv(cls, csv: str, **schema_updates) -> pl.DataFrame:
         """Parses a CSV string into a MEDS-related dataframe using the provided schema.
 
         Args:
             csv: The CSV string to parse.
-            schema: The schema to use when parsing the CSV, passed as keyword arguments. Note that timestamp
-                columns will be read as strings then converted to the requested type using the default CSV
-                timestamp format. Schema defaults are _not_ provided, and all CSV columns must have a schema
-                entry.
+            schema_updates: The schema to use when parsing the CSV, passed as keyword arguments. Note that
+                timestamp columns will be read as strings then converted to the requested type using the
+                default CSV timestamp format. Schema defaults are drawn from all the available MEDS schemas,
+                _without consideration for whether the passed csv is appropriate for that given schema_.
 
         Returns:
             A polars DataFrame with the parsed data.
 
         Raises:
-            ValueError: If the CSV cannot be read under the provided schema.
+            ValueError: If the CSV cannot be read under the provided schema and/or the schema defaults.
 
         Examples:
-            >>> MEDSDataset._parse_csv(
+            >>> MEDSDataset.parse_csv(
             ...     'subject_id,time,code,numeric_value\\n0,"1/1/2025, 12:00:00",foo,1.0',
             ...     subject_id=pl.Int64, time=pl.Datetime("us"), code=pl.String, numeric_value=pl.Float32
             ... )
@@ -423,21 +480,94 @@ class MEDSDataset:
             │ 0          ┆ 2025-01-01 12:00:00 ┆ foo  ┆ 1.0           │
             └────────────┴─────────────────────┴──────┴───────────────┘
 
-        Errors are raised when the schema is incomplete, inaccurate, or the CSV is malformed:
-            >>> MEDSDataset._parse_csv(
+        Note that schema defaults are sourced from all MEDS schemas:
+            >>> MEDSDataset.parse_csv(
             ...     'subject_id,time,code,numeric_value\\n0,"1/1/2025, 12:00:00",foo,1.0',
+            ... )
+            shape: (1, 4)
+            ┌────────────┬─────────────────────┬──────┬───────────────┐
+            │ subject_id ┆ time                ┆ code ┆ numeric_value │
+            │ ---        ┆ ---                 ┆ ---  ┆ ---           │
+            │ i64        ┆ datetime[μs]        ┆ str  ┆ f32           │
+            ╞════════════╪═════════════════════╪══════╪═══════════════╡
+            │ 0          ┆ 2025-01-01 12:00:00 ┆ foo  ┆ 1.0           │
+            └────────────┴─────────────────────┴──────┴───────────────┘
+            >>> MEDSDataset.parse_csv(
+            ...     'code,description,parent_codes\\nfoo,foobar code,"bar,baz"',
+            ... )
+            shape: (1, 3)
+            ┌──────┬─────────────┬──────────────┐
+            │ code ┆ description ┆ parent_codes │
+            │ ---  ┆ ---         ┆ ---          │
+            │ str  ┆ str         ┆ list[str]    │
+            ╞══════╪═════════════╪══════════════╡
+            │ foo  ┆ foobar code ┆ ["bar,baz"]  │
+            └──────┴─────────────┴──────────────┘
+            >>> MEDSDataset.parse_csv(
+            ...     'subject_id,split\\n0,train\\n1,test',
+            ... )
+            shape: (2, 2)
+            ┌────────────┬───────┐
+            │ subject_id ┆ split │
+            │ ---        ┆ ---   │
+            │ i64        ┆ str   │
+            ╞════════════╪═══════╡
+            │ 0          ┆ train │
+            │ 1          ┆ test  │
+            └────────────┴───────┘
+            >>> MEDSDataset.parse_csv(
+            ...     'subject_id,prediction_time,boolean_value\\n0,"1/1/2025, 12:00:00",False',
+            ... )
+            shape: (1, 3)
+            ┌────────────┬─────────────────────┬───────────────┐
+            │ subject_id ┆ prediction_time     ┆ boolean_value │
+            │ ---        ┆ ---                 ┆ ---           │
+            │ i64        ┆ datetime[μs]        ┆ bool          │
+            ╞════════════╪═════════════════════╪═══════════════╡
+            │ 0          ┆ 2025-01-01 12:00:00 ┆ false         │
+            └────────────┴─────────────────────┴───────────────┘
+
+        Note that columns are not verified to come from a single MEDS schema:
+            >>> MEDSDataset.parse_csv(
+            ...     'subject_id,prediction_time,split\\n0,"1/1/2025, 12:00:00",train',
+            ... )
+            shape: (1, 3)
+            ┌────────────┬─────────────────────┬───────┐
+            │ subject_id ┆ prediction_time     ┆ split │
+            │ ---        ┆ ---                 ┆ ---   │
+            │ i64        ┆ datetime[μs]        ┆ str   │
+            ╞════════════╪═════════════════════╪═══════╡
+            │ 0          ┆ 2025-01-01 12:00:00 ┆ train │
+            └────────────┴─────────────────────┴───────┘
+
+        Columns from MEDS schemas can also be overwritten with the schema updates keyword arguments:
+            >>> MEDSDataset.parse_csv(
+            ...     'subject_id,prediction_time,parent_codes\\n0,"1/1/2025, 12:00:00",train',
+            ...     subject_id=pl.String, prediction_time=pl.String, parent_codes=pl.String
+            ... )
+            shape: (1, 3)
+            ┌────────────┬────────────────────┬──────────────┐
+            │ subject_id ┆ prediction_time    ┆ parent_codes │
+            │ ---        ┆ ---                ┆ ---          │
+            │ str        ┆ str                ┆ str          │
+            ╞════════════╪════════════════════╪══════════════╡
+            │ 0          ┆ 1/1/2025, 12:00:00 ┆ train        │
+            └────────────┴────────────────────┴──────────────┘
+
+        Errors are raised when the schema is incomplete, inaccurate, or the CSV is malformed:
+            >>> MEDSDataset.parse_csv(
+            ...     'subject_id,time,code_2,numeric_value\\n0,"1/1/2025, 12:00:00",foo,1.0',
             ... )
             Traceback (most recent call last):
                 ...
-            ValueError: Missing columns in schema: ['subject_id', 'time', 'code', 'numeric_value']
-            >>> MEDSDataset._parse_csv(
-            ...     'subject_id,time,code,numeric_value\\n0,"1/1/2025, 12:00:00",foo,1.0',
-            ...     subject_id=pl.Int64, time=pl.Datetime("us"), code=pl.Float32, numeric_value=pl.Float32
+            ValueError: Missing schema dtype for column: code_2!
+            >>> MEDSDataset.parse_csv(
+            ...     'subject_id,time,code,numeric_value\\n0,"1/1/2025, 12:00:00",foo,foo',
             ... )
             Traceback (most recent call last):
                 ...
             ValueError: Failed to read:...
-            >>> MEDSDataset._parse_csv(123)
+            >>> MEDSDataset.parse_csv(123)
             Traceback (most recent call last):
                 ...
             ValueError: csv must be a string; got <class 'int'>
@@ -446,62 +576,50 @@ class MEDSDataset:
         if not isinstance(csv, str):
             raise ValueError(f"csv must be a string; got {type(csv)}")
 
-        read_schema = {**schema}
-
+        read_schema = {}
         time_schema = {}
-        for time_col in [time_field, prediction_time_field]:
-            if time_col in read_schema:
-                time_schema[time_col] = read_schema.pop(time_col)
-                read_schema[time_col] = pl.String
+        has_parent_codes = False
 
         cols = csv.split("\n")[0].split(",")
-        if not all(col in read_schema for col in cols):
-            raise ValueError(f"Missing columns in schema: {cols}")
+        for col in cols:
+            do_retype_time = col in cls.TIME_FIELDS
+            do_retype_parent_codes = col == parent_codes_field
+            if col in schema_updates:
+                read_schema[col] = schema_updates[col]
+                if col in cls.TIME_FIELDS and not schema_updates[col].is_temporal():
+                    do_retype_time = False
+                if col == parent_codes_field and schema_updates[col] is not cls.PL_CODE_METADATA_SCHEMA[col]:
+                    do_retype_parent_codes = False
+            elif col in cls.PL_DATA_SCHEMA:
+                read_schema[col] = cls.PL_DATA_SCHEMA[col]
+            elif col in cls.PL_CODE_METADATA_SCHEMA:
+                read_schema[col] = cls.PL_CODE_METADATA_SCHEMA[col]
+            elif col in cls.PL_SUBJECT_SPLIT_SCHEMA:
+                read_schema[col] = cls.PL_SUBJECT_SPLIT_SCHEMA[col]
+            elif col in cls.PL_LABEL_SCHEMA:
+                read_schema[col] = cls.PL_LABEL_SCHEMA[col]
+            else:
+                raise ValueError(f"Missing schema dtype for column: {col}!")
+
+            if do_retype_time:
+                time_schema[col] = read_schema.pop(col)
+                read_schema[col] = pl.String
+            elif do_retype_parent_codes:
+                has_parent_codes = True
+                read_schema[col] = pl.String
+
         try:
             df = pl.read_csv(StringIO(csv), schema={col: read_schema[col] for col in cols})
         except Exception as e:
             raise ValueError(f"Failed to read:\n{csv}\nUnder schema:\n{read_schema}") from e
-        cols = df.columns
 
-        if time_schema:
-            df = df.with_columns(
-                *[
-                    pl.col(time_col).str.strptime(schema, cls.DEFAULT_CSV_TS_FORMAT).alias(time_col)
-                    for time_col, schema in time_schema.items()
-                ]
+        col_updates = {t: pl.col(t).str.strptime(dt, cls.CSV_TS_FORMAT) for t, dt in time_schema.items()}
+        if has_parent_codes:
+            col_updates[parent_codes_field] = (
+                pl.col(parent_codes_field).str.split(", ").cast(pl.List(pl.String))
             )
-        return df.select(cols)
 
-    @classmethod
-    def parse_data_csv(cls, csv: str, **schema_updates) -> pl.DataFrame:
-        """Converts a string or dict of named strings to a MEDS DataFrame by interpreting them as CSVs."""
-
-        return cls._parse_csv(csv, **cls.PL_DATA_SCHEMA, **schema_updates)
-
-    @classmethod
-    def parse_code_metadata_csv(cls, csv: str) -> pl.DataFrame:
-        """Converts a string to a code metadata DataFrame by interpreting it as a CSV."""
-
-        read_schema = {**cls.PL_CODE_METADATA_SCHEMA}
-
-        parent_codes_real = read_schema.pop(parent_codes_field)
-        read_schema[parent_codes_field] = pl.String
-
-        df = cls._parse_csv(csv, **read_schema)
-
-        if parent_codes_field in df.columns:
-            cols = df.columns
-            df = df.with_columns(
-                pl.col(parent_codes_field).str.split(", ").cast(parent_codes_real).alias(parent_codes_field)
-            ).select(cols)
-
-        return df
-
-    @classmethod
-    def parse_subject_splits_csv(cls, csv: str) -> pl.DataFrame:
-        """Converts a string to a subject splits DataFrame by interpreting it as a CSV."""
-
-        return cls._parse_csv(csv, **cls.PL_SUBJECT_SPLIT_SCHEMA)
+        return df.with_columns(**col_updates).select(cols)
 
     @classmethod
     def from_yaml(cls, yaml: str | Path) -> "MEDSDataset":
@@ -717,25 +835,18 @@ class MEDSDataset:
 
             if root == "data":
                 rest = "/".join(key_parts[1:])
-                data_shards[rest.replace(".parquet", "")] = cls.parse_data_csv(value)
+                data_shards[rest.replace(".parquet", "")] = cls.parse_csv(value)
+            elif key == code_metadata_filepath:
+                code_metadata = cls.parse_csv(value)
+            elif key == subject_splits_filepath:
+                subject_splits = cls.parse_csv(value)
+            elif key == dataset_metadata_filepath:
+                dataset_metadata = DatasetMetadata(**json.loads(value))
             else:
-                if key == code_metadata_filepath:
-                    code_metadata = cls.parse_code_metadata_csv(value)
-                elif key == subject_splits_filepath:
-                    subject_splits = cls.parse_subject_splits_csv(value)
-                elif key == dataset_metadata_filepath:
-                    dataset_metadata = DatasetMetadata(**json.loads(value))
-                else:
-                    raise ValueError(f"Unrecognized key in YAML: {key}")
+                raise ValueError(f"Unrecognized key in YAML: {key}")
 
         if len(data_shards) == 0:
             raise ValueError("No data shards found in YAML")
-
-        if code_metadata is None:
-            code_metadata = pl.DataFrame(
-                {code_field: [], description_field: [], parent_codes_field: []},
-                schema=cls.PL_CODE_METADATA_SCHEMA,
-            )
 
         return cls(
             data_shards=data_shards,
