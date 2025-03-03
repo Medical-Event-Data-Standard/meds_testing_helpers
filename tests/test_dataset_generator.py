01e2d4f92e39260fc4ec3f97deafb4c4515cc525
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-03 14:02:01 -0500

    Ensured basics of pytest plugin are tracked properly in coverage reporting.

--- tests/test_dataset_generator.py
+++ tests/test_dataset_generator.py
@@ -7,6 +7,11 @@ from meds_testing_helpers.dataset import MEDSDataset
 #    MEDSDataset(root_dir=generated_mimic_like_MEDS)
 
 
+# def test_force_plugin_reload():
+# """Ensures pytest_plugin.py is loaded after coverage tracking starts."""
+# importlib.reload(meds_testing_helpers.pytest_plugin)
+
+
 def test_dataset_generation(generated_sample_MEDS):
     """Test the generation of a dataset."""
 
